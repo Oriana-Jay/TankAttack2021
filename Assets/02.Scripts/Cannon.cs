6a89c8af6e02527ecd64d79965bc0b7d1145f1b5
Lee Jae Hyun <myleje@gmail.com>
2021-04-27 13:44:19 +0900

    .

--- Assets/02.Scripts/Cannon.cs
+++ Assets/02.Scripts/Cannon.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 2000.0f;
+
     void Start()
     {
-        
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed);        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
