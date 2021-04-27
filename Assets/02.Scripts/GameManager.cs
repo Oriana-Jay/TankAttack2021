0077e70df304cf2033fce7fc741ad895da833986
Lee Jae Hyun <myleje@gmail.com>
2021-04-27 16:27:39 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -9,9 +9,9 @@ public class GameManager : MonoBehaviour
     {
         //PhotonNetwork.IsMessageQueueRunning = true;
 
-        Vector3 pos = new Vector3(Random.Range(-200.0f, 200.0f),
+        Vector3 pos = new Vector3(Random.Range(-150.0f, 150.0f),
                                   5.0f,
-                                  Random.Range(-200.0f, 200.0f));
+                                  Random.Range(-150.0f, 150.0f));
 
         // 통신이 가능한 주인공 캐릭터(탱크) 생성
         PhotonNetwork.Instantiate("Tank",
